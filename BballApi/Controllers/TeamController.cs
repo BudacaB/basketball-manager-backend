5db3a11f6be37ef02906481c4a5960dff52be91f
BudacaB <budaca.b@gmail.com>
2019-11-07 22:40:47 +0200

    started controller

---
+++ BballApi/Controllers/TeamController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BballApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BballApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TeamController : ControllerBase
+    {
+        // GET: api/Team
+        [HttpGet]
+        public Player Get()
+        {
+            return new Player { FirstName = "test" };
+        }
+
+        // GET: api/Team/5
+        //[HttpGet("{id}", Name = "Get")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST: api/Team
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT: api/Team/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE: api/ApiWithActions/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
