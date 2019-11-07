d71a50270abdb13c82af50eed9753bbe12e631a3
BudacaB <budaca.b@gmail.com>
2019-11-07 17:45:34 +0200

    player model class

---
+++ BballApi/Models/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BballApi.Models
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Position { get; set; }
+        public int Age { get; set; }
+        public int Weight { get; set; }
+        public string College { get; set; }
+        public decimal Salary { get; set; }
+        public int Stamina { get; set; }
+        public int Speed { get; set; }
+        public int Strength { get; set; }
+        public bool Injured { get; set; }
+    }
+}
