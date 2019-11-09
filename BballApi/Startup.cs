85993dabb4b472cd3075a8b9f1b23dde3c26e070
BudacaB <budaca.b@gmail.com>
2019-11-09 21:26:21 +0200

    trying to add swagger

--- BballApi/Startup.cs
+++ BballApi/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace BballApi
 {
@@ -26,6 +27,11 @@ namespace BballApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            //services.AddSwaggerGen(c =>
+            //{
+            //    c.SwaggerDoc("v1", new Info { Title = "bball api", Version = "v1" });
+            //});
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +52,12 @@ namespace BballApi
             {
                 endpoints.MapControllers();
             });
+
+            //app.UseSwagger();
+            //app.UseSwaggerUI(c =>
+            //{
+            //    c.SwaggerEndpoint("/swagger/", "Bball Api V1");
+            //});
         }
     }
 }
